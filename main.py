698b20cc55e5b3d03f2b61c859a0bcc7bfb12251
huai zhu <138862916+riceshowerX@users.noreply.github.com>
2024-07-30 16:12:35 +0800

    2.6

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 # main.py
-from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.properties import StringProperty, NumericProperty
-from kivy.lang import Builder
-from kivy.clock import Clock
-from core import ImageProcessor, Config
-from kivy.core.window import Window
 import os
-from kivy.utils import platform
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivy.properties import StringProperty, NumericProperty
+from kivy.uix.filechooser import FileChooserIconView, FileChooserListView
+from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from core import ImageProcessor, Config
+from kivy_ui.main_window import MainWindow
 
 # 加载 KV 文件
 Builder.load_file("kivy_ui/main_window.kv")
 
 Window.clearcolor = (1, 1, 1, 1)  # 设置背景颜色为白色
 
-class MainWindow(Widget):
+class SnapForgeApp(App):
     source_directory = StringProperty(None)
     target_directory = StringProperty(None)
     source_file = StringProperty(None)
@@ -22,56 +25,47 @@ class MainWindow(Widget):
     config = Config()
     image_processor = ImageProcessor(config)
 
-    def select_source(self, instance):
-        if platform == 'android':
-            # Android平台文件选择
-            from jnius import autoclass
-            Intent = autoclass('android.content.Intent')
-            FileChooser = autoclass('androidx.activity.result.contract.ActivityResultContracts$StartActivityForResult')
-            startActivityForResult = autoclass('androidx.activity.ComponentActivity').registerForActivityResult(
-                FileChooser(), self.on_file_selected)
-            intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-            intent.addCategory(Intent.CATEGORY_OPENABLE)
-            intent.setType("*/*")
-            startActivityForResult.launch(intent)
-        else:
-            # 其他平台文件选择
-            self.source_file =  os.path.abspath(os.path.expanduser(os.path.join('~', "Documents")))
-            self.ids.source_button.text = '已选择: {}'.format(self.source_file)
+    def build(self):
+        self.root = MainWindow()
+        self.root.ids.source_button.bind(on_press=self.select_source)
+        self.root.ids.target_button.bind(on_press=self.select_target_directory)
+        self.root.ids.rename_button.bind(on_press=self.rename_files)
+        self.root.ids.convert_button.bind(on_press=self.convert_files)
+        self.root.ids.compress_button.bind(on_press=self.compress_files)
+        return self.root
 
-    def on_file_selected(self, data):
-        if data is not None:
-            self.source_file = data.getDataString()
-            self.ids.source_button.text = '已选择: {}'.format(self.source_file)
+    def select_source(self, instance):
+        content = FileChooserIconView()
+        content.bind(on_submit=self._on_file_selected)
+        self._popup = Popup(title="选择图片文件", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def _on_file_selected(self, filechooser, selection, *args):
+        if selection:
+            self.source_file = os.path.abspath(selection[0])
+            self.root.ids.source_button.text = f'已选择: {self.source_file}'
+        self._popup.dismiss()
 
     def select_target_directory(self, instance):
-        if platform == 'android':
-            # Android平台文件夹选择
-            from jnius import autoclass
-            Intent = autoclass('android.content.Intent')
-            FileChooser = autoclass('androidx.activity.result.contract.ActivityResultContracts$StartActivityForResult')
-            startActivityForResult = autoclass('androidx.activity.ComponentActivity').registerForActivityResult(
-                FileChooser(), self.on_directory_selected)
-            intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
-            startActivityForResult.launch(intent)
-        else:
-            # 其他平台文件夹选择
-            self.target_directory = os.path.abspath(os.path.expanduser(os.path.join('~', "Documents")))
-            self.ids.target_button.text = '已选择: {}'.format(self.target_directory)
+        content = FileChooserListView(path='/', filters=['*'])
+        content.bind(on_submit=self._on_directory_selected)
+        self._popup = Popup(title="选择保存图片文件夹", content=content, size_hint=(0.9, 0.9))
+        self._popup.open()
 
-    def on_directory_selected(self, data):
-        if data is not None:
-            self.target_directory = data.getDataString()
-            self.ids.target_button.text = '已选择: {}'.format(self.target_directory)
+    def _on_directory_selected(self, filechooser, selection, *args):
+        if selection:
+            self.target_directory = os.path.abspath(selection[0])
+            self.root.ids.target_button.text = f'已选择: {self.target_directory}'
+        self._popup.dismiss()
 
     def rename_files(self, instance):
         if not self.source_file or not self.target_directory:
             self.show_error_message("请先选择原始图片文件和保存图片文件夹！")
             return
 
-        prefix = self.ids.prefix_input.text
+        prefix = self.root.ids.prefix_input.text
         try:
-            start_number = int(self.ids.start_number_input.text)
+            start_number = int(self.root.ids.start_number_input.text)
         except ValueError:
             self.show_error_message("起始编号必须是一个整数！")
             return
@@ -91,7 +85,7 @@ class MainWindow(Widget):
             self.show_error_message("请先选择原始图片文件和保存图片文件夹！")
             return
 
-        target_format = self.ids.format_input.text
+        target_format = self.root.ids.format_input.text
         if not target_format:
             self.show_error_message("请指定目标格式！")
             return
@@ -111,7 +105,7 @@ class MainWindow(Widget):
             return
 
         try:
-            quality = int(self.ids.quality_input.text)
+            quality = int(self.root.ids.quality_input.text)
         except ValueError:
             self.show_error_message("压缩质量必须是一个整数！")
             return
@@ -127,10 +121,22 @@ class MainWindow(Widget):
 
     def update_progress(self, current, total):
         self.progress_value = (current / total) * 100
+        self.root.ids.progress_bar.value = self.progress_value
 
     def show_info_message(self, message):
-        self.ids.status_label.text = message
+        self.root.ids.status_label.text = message
 
     def show_error_message(self, message):
-        self.ids.status_label.text = message
-        # 可以使用其他方式显示错误信息，例如弹窗
+        content = BoxLayout(orientation='vertical')
+        content.add_widget(Label(text=message))
+        close_button = Button(text="关闭")
+        close_button.bind(on_press=self._close_popup)
+        content.add_widget(close_button)
+        self._popup = Popup(title="错误", content=content, size_hint=(0.8, 0.4))
+        self._popup.open()
+
+    def _close_popup(self, instance):
+        self._popup.dismiss()
+
+if __name__ == "__main__":
+    SnapForgeApp().run()
